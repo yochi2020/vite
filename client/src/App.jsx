6e6d6c9043511c899a0e27c34f8d6e4ac6edb901
chi.suz <yochi.suz@gmail.com>
2023-11-10 21:02:25 +0700

    create and run server

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter ,Route, Routes} from "react-router-dom"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import SignOut from "./pages/SignOut"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import Header from "./components/Header"
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-out" element={<SignOut/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
