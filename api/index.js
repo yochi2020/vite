4da5f52da32e7504096723e5a388091443bcb3a8
chi.suz <yochi.suz@gmail.com>
2023-11-11 00:02:10 +0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config()
+mongoose.connect(process.env.MONGO).then(()=>{
+  console.log("Connected to MongoDB")
+}).catch(err => console.log(err))
+const app = express();
 
-const app =  express();
-
-app.listen(3001,()=>{
-    console.log("Server is running on port 3001");
-})
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
